5953448c167302ddeb0db6497e195d442ae2938a
Ruben Amortegui <ruben.amortegui@gmail.com>
2017-12-21 01:09:15 -0700

    Display shouts and add views of users and shout.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,13 @@
 class UsersController < Clearance::UsersController
+  def show
+    @user = User.find(params[:id])
+    @shouts = @user.shouts
+  end
+
   private
+
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
+
 end
