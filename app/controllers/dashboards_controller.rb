4264b2d61720174140a74e3b5572996126320494
Ruben Amortegui <ruben.amortegui@gmail.com>
2018-01-04 11:47:44 -0700

    Extract Timeline and Dashboard classes
    Using Single Responsibility Principle

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,7 +1,6 @@
 class DashboardsController < ApplicationController
   def show
     @shout = Shout.new
-    @shouts = current_user.timeline_shouts
-    render
+    @dashboard = Dashboard.new(current_user)
   end
 end
